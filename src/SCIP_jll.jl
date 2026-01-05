d3c5b5c266f4ee6d380fce9be92ba0e6be6b2c7f
jlbuild <juliabuildbot@gmail.com>
2026-01-05 14:32:13 +0000

    SCIP_jll build 1000.0.1+0

--- src/SCIP_jll.jl
+++ src/SCIP_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("SCIP")
-JLLWrappers.@generate_main_file("SCIP", UUID("e5ac4fe4-a920-5659-9bf8-f9f73e9e79ce"))
+JLLWrappers.@generate_main_file("SCIP", Base.UUID("e5ac4fe4-a920-5659-9bf8-f9f73e9e79ce"))
 end  # module SCIP_jll
